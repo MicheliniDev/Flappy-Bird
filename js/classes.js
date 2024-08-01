cd68d47757a55667f6d5bdae71ac48e244efeb17
MicheliniDev <michelinidev@gmail.com>
2024-08-01 12:15:02 -0300

    Euteodeioaundermauricio

---
+++ js/classes.js
@@ -0,0 +1,13 @@
+class Obj {
+    constructor(posx, posy, largura, altura, imagem) {
+        this.posx = posx 
+        this.posy = posy
+        this.altura = altura 
+        this.largura = largura
+        this.imagem = imagem
+    }
+
+    draw() {
+        let img = new Image()
+    }
+}
