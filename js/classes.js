b3eddbffb8228c98527d5e4b98ff7131de317e7a
MicheliniDev <michelinidev@gmail.com>
2024-08-07 10:38:25 -0300

    atualização no cenário

--- js/classes.js
+++ js/classes.js
@@ -1,13 +1,25 @@
-class Obj {
-    constructor(posx, posy, largura, altura, imagem) {
-        this.posx = posx 
+
+class obj{
+    constructor(posx,posy,largura,altura,imagem){
+        this.posx = posx
         this.posy = posy
-        this.altura = altura 
         this.largura = largura
+        this.altura = altura
         this.imagem = imagem
     }
 
-    draw() {
+    draw(){
         let img = new Image()
+        img.src = this.imagem
+        canvas.drawImage(img, this.posx, this.posy, this.largura, this.altura)
+}
+}
+
+class Bg extends obj{
+    move(velocidade, limite, posicao){
+        this.posx -= velocidade
+        if(this.posx < limite){
+            this.posx = posicao
+        }
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-const canvas = document.getElementById("canvas").getContext("2d")
+const canvas = document.getElementById('canvas').getContext('2d')
+
+let bg = new Bg(0, 0, 500, 900,'img/sky.png')
+let bg2 = new Bg(500, 0, 500, 900,'img/sky.png')
+let ground = new Bg(0,700,500,200, 'img/ground.png')
+let ground2 = new Bg(0,700,500,200, 'img/ground.png')
+
+function draw(){
+    bg.draw()
+    bg2.draw()
+    ground.draw()
+    ground2.draw()
+}
+
+function update(){
+    bg.move(10, -500,0)
+    bg2.move(10,0,500)
+    ground.move(10, -500,0)
+    ground2.move(10,0,485)
+}
+
+function main(){
+    canvas.clearRect(0,0,500,900)
+    update()
+    draw()
+}
+
+setInterval(main, 100)
