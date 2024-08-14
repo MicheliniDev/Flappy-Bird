d6c525eac9708d4b4e25cbc79ff3c3ca2b9e7936
MicheliniDev <michelinidev@gmail.com>
2024-08-14 10:44:26 -0300

    animando o pássaro

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,8 @@
 
 class obj{
+    quadro = 0
+    tempo = 0
+
     constructor(posx,posy,largura,altura,imagem){
         this.posx = posx
         this.posy = posy
@@ -13,6 +16,17 @@ class obj{
         img.src = this.imagem
         canvas.drawImage(img, this.posx, this.posy, this.largura, this.altura)
 }
+    animacao(vel, limite, nomeImg){
+        this.tempo += 1
+        if (this.tempo >= vel) {
+            this.tempo = 0
+            this.quadro += 1
+        }
+        if(this.quadro >= limite){
+            this.quadro = 0
+        }
+        this.imagem = 'img/'+nomeImg+this.quadro+'.png'
+    }
 }
 
 class Bg extends obj{
@@ -22,4 +36,12 @@ class Bg extends obj{
             this.posx = posicao
         }
     }
+}
+
+class Ground extends Bg{
+
+}
+
+class Bird extends obj {
+
 }
--- js/script.js
+++ js/script.js
@@ -2,27 +2,32 @@ const canvas = document.getElementById('canvas').getContext('2d')
 
 let bg = new Bg(0, 0, 500, 900,'img/sky.png')
 let bg2 = new Bg(500, 0, 500, 900,'img/sky.png')
-let ground = new Bg(0,700,500,200, 'img/ground.png')
-let ground2 = new Bg(0,700,500,200, 'img/ground.png')
+let ground = new Ground(0,700,500,200, 'img/ground.png')
+let ground2 = new Ground(500,700,500,200, 'img/ground.png')
+let bird = new Bird(50, 400, 63, 51, "img/bird0.png")
 
 function draw(){
     bg.draw()
     bg2.draw()
     ground.draw()
     ground2.draw()
+    bird.draw()
 }
 
 function update(){
-    bg.move(10, -500,0)
-    bg2.move(10,0,500)
-    ground.move(10, -500,0)
-    ground2.move(10,0,485)
+    bg.move(2, -500,0)
+    bg2.move(2,0,500)
+    ground.move(1, -500,0)
+    ground2.move(1,0,500)
+
+    bird.animacao(7, 4, 'bird')
 }
 
 function main(){
     canvas.clearRect(0,0,500,900)
     update()
     draw()
+    requestAnimationFrame(main)
 }
 
-setInterval(main, 100)
+main()
