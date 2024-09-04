c3911c3aab9f5c85a4bea16b0d7ca9d3e93baad9
MicheliniDev <michelinidev@gmail.com>
2024-09-04 10:41:56 -0300

    adicionando os canos

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ let bg2 = new Bg(500, 0, 500, 900,'img/sky.png')
 let ground = new Ground(0,700,500,200, 'img/ground.png')
 let ground2 = new Ground(500,700,500,200, 'img/ground.png')
 let bird = new Bird(50, 400, 63, 51, "img/bird0.png")
+let cano1 = new obj(300,500,96,358,"img/pipe1.png")
+let cano2 = new obj(300,0,96,358,"img/pipe2.png")
 
 document.addEventListener('click', function(e){
     bird.velocidade -= 12
@@ -16,6 +18,8 @@ function draw(){
     ground.draw()
     ground2.draw()
     bird.draw()
+    cano1.draw()
+    cano2.draw()
 }
 
 function update(){
